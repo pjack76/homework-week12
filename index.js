15ad3c4612468718cd905c74854a7030574285f7
Jack Long <putheara.long@gmail.com>
2020-04-29 03:43:21 -0400

    add prompts working somewhat

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const inq = require("inquirer");
 const mysql = require("mysql");
+//const showTable = require('console.table');
 
 const database_conn = mysql.createConnection({
   host: "localhost",
@@ -17,4 +18,71 @@ database_conn.connect(function(err){
 
     console.log("Database connected!");
 
-})
+});
+
+const questions = [
+{   name: "selection",
+    type: "list",
+    message: "What would you like to do?",
+    choices: ["Add employee", "Update role", "View all employees by department", "View employees by role", "Done"]
+},
+{   name: "firstName",
+    type: "input",
+    message: "Please enter the first name of the employee.",
+    when: function (answer){
+        return answer.selection === "Add employee"
+    }
+},
+
+{   name: "lastName",
+    type: "input",
+    message: "Please enter the last name of the employee.",
+    when: function (answer){
+        return answer.selection === "Add employee"
+    }
+},
+
+{   name: "role",
+    type: "input",
+    message: "Please enter the role of the employee.",
+    when: function (answer){
+        return answer.selection === "Add employee"
+    }
+}
+];
+
+
+function userInput(){
+    inq.prompt(questions).then(function(answer){
+        
+       /* switch (anwer.selection){
+            case "Add employee":
+
+            break;
+
+            case "Update role":
+
+            break;
+
+            case "View all employee by department":
+
+            break;
+
+            case "View all employee by role":
+
+            break;
+
+            case "Done":
+
+            break;
+        }*/
+        console.log(answer);
+    })
+};
+
+userInput();
+
+//function addEmployee(){
+
+
+//}
